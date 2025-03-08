f3dbd7076f2a445e0e65b49bf7c05e40477cb19d
Joseph Misiti <joseph.misiti@mathandpencil.com>
2025-03-08 14:36:29 -0500

    Add Home.module.scss for styling and refactor Home component layout and content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,63 @@
 import Image from "next/image";
+import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>hi i’m joe.</h1>
+        <p>
+          i’m currently running math & pencil, a boutique full-stack software
+          and data science consultancy. I have been running it since 2011, and
+          am not looking for full-time employment (unless you can convince me to
+          quit of course). reach out if you need any help (ios/python/ml)
+        </p>
+        <p>
+          i’m a full-stack software engineer + data scientist living in the
+          lower east side, nyc
+        </p>
+        <p>i specialize in machine learning</p>
+        <p>
+          my current interests are the following distributed computing, bayesian
+          inference, data mining, computer vision, guitar, piano, django,
+          golang, hadoop, spark, asynchronous-io, wavelets, signal processing,
+          harmonica, history, react.js, books, traveling, restaurants, cooking,
+          alcohol, running, red wine(montepulciano), white wine (torrontes),
+          casamigos blanco,
+        </p>
+        <p>
+          i keep track of what i’m reading <a href="#">here</a>
+        </p>
+        <p>
+          i keep a technical blog <a href="#">here</a>
+        </p>
+        <p>
+          i write about web development <a href="#">here</a>
+        </p>
+        <p>
+          you can find my on github <a href="#">here</a>
+        </p>
+        <p>
+          you can find my on twitter <a href="#">here</a>
+        </p>
+        <p>
+          i love to read, here is a list what i read this year{" "}
+          <a href="#">here</a>
+        </p>
+        <p>i do not use facebook</p>
+        <p className={styles.italic}>
+          if you need any data help, ios help, backend help, or have a good book
+          recommendation, please contact me at joseph dot misiti at
+          mathandpencil dot com
+        </p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className={styles.footer}>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://mathandpencil.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Learn More
         </a>
       </footer>
     </div>
