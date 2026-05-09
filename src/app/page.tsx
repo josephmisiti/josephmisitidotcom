c426b31c6902f10995796568aa8047e30fc5e6a1
Joseph Misiti <joseph.misiti@mathandpencil.com>
2026-05-09 15:15:55 -0400

    Remove personal interests list from homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,64 +30,6 @@ export default function Home() {
           </a>
         </p>
         <p>
-          my current interests are the following{" "}
-          <a href="https://en.wikipedia.org/wiki/Distributed_computing">
-            distributed computing
-          </a>
-          ,{" "}
-          <a href="https://en.wikipedia.org/wiki/Bayesian_inference">
-            bayesian inference
-          </a>
-          , <a href="https://en.wikipedia.org/wiki/Data_mining">data mining</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/Computer_vision">
-            computer vision
-          </a>
-          ,{" "}
-          <a href="https://en.wikipedia.org/wiki/C._F._Martin_%26_Company">
-            guitar
-          </a>
-          , <a href="https://en.wikipedia.org/wiki/Piano">piano</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/Django_(web_framework)">
-            django
-          </a>
-          ,{" "}
-          <a href="https://en.wikipedia.org/wiki/Go_(programming_language)">
-            golang
-          </a>
-          , <a href="https://en.wikipedia.org/wiki/Apache_Hadoop">hadoop</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/Apache_Spark">spark</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/Asynchronous_I/O">
-            asynchronous-io
-          </a>
-          , <a href="https://en.wikipedia.org/wiki/Wavelets">wavelets</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/Signal_Processing">
-            signal processing
-          </a>
-          , <a href="https://en.wikipedia.org/wiki/Harmonica">harmonica</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/History">history</a>,{" "}
-          <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)">
-            react.js
-          </a>
-          ,{" "}
-          <a href="https://github.com/josephmisiti/reading/tree/master/books/">
-            books
-          </a>
-          , <a href="#">traveling</a>, <a href="#">restaurants</a>,{" "}
-          <a href="#">cooking</a>, <a href="#">alcohol</a>,{" "}
-          <a href="#">running</a>,{" "}
-          <a href="https://www.wineterroirs.com/2010/08/dard_et_ribo.html">
-            syrah (dard et ribo)
-          </a>
-          ,{" "}
-          <a href="https://www.youtube.com/watch?v=bntz_xnJ1UQ">
-            poulsard (domaine cavarodes)
-          </a>
-          ,{" "}
-          <a href="https://www.youtube.com/watch?v=EbG6OEQ54PQ&t=327s">
-            macerated blends (domaine matassa)
-          </a>
-        </p>
-        <p>
           i keep track of what i’m reading{" "}
           <a href="https://delicious.com/josephmisiti">here</a>
         </p>
